a8d89ddd26659fc1c80329a99ce454148eea41f5
Willem Staels <willem@epicbrands.be>
2017-02-08 16:15:40 +0100

    Add forever config file + log directory

---
+++ forever.js
@@ -0,0 +1,18 @@
+[
+  {
+    //Player Service
+    "ERRFILE": "/var/xscreen-player/Logs/xs_play_error.log",
+    "append": true,
+    "watch": true,
+    "script": "playerService.js",
+    "sourceDir": "/var/xscreen-player/Services"
+  },
+  {
+    //Sync Service
+    "ERRFILE": "/var/xscreen-player/Logs/xs_sync_error.log",
+    "append": true,
+    "watch": true,
+    "script": "syncService.js",
+    "sourceDir": "/var/xscreen-player/Services"
+  }
+]
