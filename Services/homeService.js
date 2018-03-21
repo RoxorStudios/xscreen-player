ec30010e874d3fa99948ee6e4a46ac77372d5a62
Willem Staels <willemstaels@MacBook-Pro-van-Willem-2.local>
2018-03-21 14:10:11 +0100

    Update home calling route

--- Services/homeService.js
+++ Services/homeService.js
@@ -2,20 +2,44 @@ require('dotenv').config();
 
 const unirest       = require('unirest');
 const reboot        = require('nodejs-system-reboot');
+const getIP         = require('external-ip')();
+const internalIp    = require('internal-ip');
+const procStats     = require('process-stats');
 
 var displayKey      = process.env.DISPLAY_KEY;
 var endpoint        = process.env.LIVE;
 
-var syncInterval    = 2; //Seconds
+var syncInterval    = 10; //Seconds
 var syncTimeout;
+
+var systemData = {
+    'ip_remote': null,
+    'ip_local': null
+}
 
 function sync() {
 
     clearTimeout(syncTimeout);
+    
+    //Get external ip
+    getIP((err, ip) => {
+        if (!err) {
+            systemData.ip_remote = ip
+        }
+    });
+    //Get local ip
+    internalIp.v4().then(ip => {
+        systemData.ip_local = ip
+    });
+
+    //System stats
+    var systemStats = procStats();
+    systemData.uptime = systemStats.uptime.pretty;
 
     unirest.get(endpoint + 'home/' + displayKey)
     .headers({'Accept': 'application/json', 'Content-Type': 'application/json'})
     .timeout(10000)
+    .query(systemData)
     .send()
     .end(function (response) {
         if(response.statusType == 2 && isJson(response.body)) {
