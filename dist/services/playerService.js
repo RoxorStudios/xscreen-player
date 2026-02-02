dc575a1e5f738924c763f6a053713ee1eb997712
MaartenPeeters <maarten@roxorstudios.com>
2026-02-02 10:44:49 +0100

    Unfinished: use https for xscreen endpoint

--- dist/services/playerService.js
+++ dist/services/playerService.js
@@ -39,7 +39,7 @@ app.get('/config', function (req, res) {
     res.setHeader('Content-Type', 'application/json');
     res.send(JSON.stringify({
         domain: 'xscreen.io',
-        live: 'http://xscreen.io/live/',
+        live: 'https://xscreen.io/live/',
         contentPath: process.env.CONTENT_PATH,
         socket: process.env.SOCKET,
         displayKey : process.env.DISPLAY_KEY,
